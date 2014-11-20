b34ec4e6f222adccd34f85e3734347335c82c108
Nick <nfenske@gmail.com>
2014-11-19 22:07:17 -0600

    Raked Design table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119014722) do
+ActiveRecord::Schema.define(version: 20141120034102) do
+
+  create_table "designs", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "name"
